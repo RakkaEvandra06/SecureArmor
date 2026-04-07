644131bfc77d763f4f5c05c6fa9f9870f068a34b
RakkaEvandra06 <rakkaevandra@gmail.com>
2026-04-07 18:41:51 +0700

    Update __all__ to include __version__ in passcheck module

--- passcheck/__init__.py
+++ passcheck/__init__.py
@@ -1,9 +1,5 @@
-"""
-passcheck — Password Strength Analyser
-"""
-
 from .analyzer import PasswordAnalyzer
 from .models import CriterionResult, PasswordAnalysis
 
-__all__ = ["PasswordAnalyzer", "PasswordAnalysis", "CriterionResult"]
+__all__ = ["PasswordAnalyzer", "PasswordAnalysis", "CriterionResult", "__version__"]
 __version__ = "3.0"
