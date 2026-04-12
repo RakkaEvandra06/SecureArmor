e54ccc91fb30042562915f5c306e4331f2cd77b7
RakkaEvandra06 <rakkaevandra@gmail.com>
2026-04-12 22:54:08 +0700

    Reorder __version__ and __all__ definitions for consistency and clarity

--- passcheck/__init__.py
+++ passcheck/__init__.py
@@ -1,5 +1,6 @@
 from .analyzer import PasswordAnalyzer
 from .models import CriterionResult, PasswordAnalysis
 
-__all__ = ["PasswordAnalyzer", "PasswordAnalysis", "CriterionResult", "__version__"]
-__version__ = "3.0"
+__version__: str = "3.0"
+
+__all__ = ["PasswordAnalyzer", "PasswordAnalysis", "CriterionResult"]
