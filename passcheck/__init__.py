fa388a2b03946f55fd20eacc68cbbe008cdf55ed
RakkaEvandra06 <rakkaevandra@gmail.com>
2026-04-23 22:28:39 +0700

    __init__.py: improve formatting of __all__ and add missing newline

--- passcheck/__init__.py
+++ passcheck/__init__.py
@@ -1,7 +1,14 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .analyzer import PasswordAnalyzer
 from .models import CriterionResult, PasswordAnalysis
 from .scoring import max_possible_score
 
 __version__: str = "3.0"
 
-__all__ = ["PasswordAnalyzer", "PasswordAnalysis", "CriterionResult", "max_possible_score"]
+__all__ = [
+    "PasswordAnalyzer",
+    "PasswordAnalysis",
+    "CriterionResult",
+    "max_possible_score",
+]
