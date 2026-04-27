a4134f3eda86a36b00adaebeb807491e905103e4
RakkaEvandra06 <rakkaevandra@gmail.com>
2026-04-27 21:52:51 +0700

    Fix version handling in __init__.py to dynamically retrieve package version

--- passcheck/__init__.py
+++ passcheck/__init__.py
@@ -1,12 +1,16 @@
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import PackageNotFoundError, version as _metadata_version
 
 from .analyzer import PasswordAnalyzer
 from .models import CriterionResult, PasswordAnalysis
 from .scoring import max_possible_score
 
-__version__: str = "3.0"
+try:
+    __version__: str = _metadata_version("securearmor")
+except PackageNotFoundError:
+    __version__ = "0.0.0+dev"
 
 __all__ = [
+    "__version__",
     "PasswordAnalyzer",
     "PasswordAnalysis",
     "CriterionResult",
