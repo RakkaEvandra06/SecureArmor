41a4db6fea3fed827621fa41a5cd60694e23ffc8
RakkaEvandra06 <rakkaevandra@gmail.com>
2026-04-29 23:32:43 +0700

    Fix package name in version retrieval and update exports in __init__.py

--- passcheck/__init__.py
+++ passcheck/__init__.py
@@ -2,10 +2,10 @@ from importlib.metadata import PackageNotFoundError, version as _metadata_versio
 
 from .analyzer import PasswordAnalyzer
 from .models import CriterionResult, PasswordAnalysis
-from .scoring import max_possible_score
+from .scoring import AnalysisSummary, criteria_summary, max_possible_score
 
 try:
-    __version__: str = _metadata_version("securearmor")
+    __version__: str = _metadata_version("passcheck")
 except PackageNotFoundError:
     __version__ = "0.0.0+dev"
 
@@ -14,5 +14,7 @@ __all__ = [
     "PasswordAnalyzer",
     "PasswordAnalysis",
     "CriterionResult",
+    "AnalysisSummary",
+    "criteria_summary",
     "max_possible_score",
 ]
